82abd5a5e1c012d204e400524c8b9cc00523a223
Starmania <wycvhrt6vzscfpedxr@gmail.com>
2025-05-28 15:45:45 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "src/Carte.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    Carte* carte = new CarteAller("Gare jaune");
+    Carte* carte2 = new CarteStand(Couleur::ORANGE);
+
+    std::cout << carte->to_string() << std::endl;
+    std::cout << carte2->to_string() << std::endl;
+
+    delete carte;
+    delete carte2;
+
+    return 0;
+}
